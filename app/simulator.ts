0efcbc2229c32859e1034a6a6318512fa296aaab
hedlivett <hedl.ivett@students.jedlik.eu>
2026-05-19 12:25:35 +0200

    ..

--- app/simulator.ts
+++ app/simulator.ts
@@ -7,55 +7,53 @@ export default class Simulator {
   #jelenlegiJatekos: number = 1;
 
   #szomszedokSzama(sorIndex: number, oszlopIndex: number, jatekosSzam: number): number {
+    const m: number[][] = this.#matrix;
+    let szomszedok: number = 0;
 
-        const m: number[][] = this.#matrix;
-        let szomszedok: number = 0;
-
-        for (let i = -1; i <= 1; i++) {
-            for (let j = -1; j <= 1; j++) {
-
-                if (i == 0 && j == 0) {
-                    continue;
-                }
-
-                if (m[sorIndex + i][oszlopIndex + j] == jatekosSzam) {
-                    szomszedok++;
-                }
-            }
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        if (i == 0 && j == 0) {
+          continue;
         }
 
-        return szomszedok;
-    }
-
-
-
-  get #kovetkezoAllapot(): number[][]{
-        const m: number[][] = this.#matrix;
-        const mFinal: number[][] = this.#matrix;
-        for (let sorIndex = 0; sorIndex < this.#matrix.length; sorIndex++) {
-            for (let oszlopIndex = 0; oszlopIndex < this.#matrix[0].length; oszlopIndex++) {
-                const szomszédok1: number = this.#szomszedokSzama(sorIndex, oszlopIndex, 1);
-                const szomszédok2: number = this.#szomszedokSzama(sorIndex, oszlopIndex, 2);
-                const szomszédokOssz: number = szomszédok1 + szomszédok2;
-                if (m[sorIndex][oszlopIndex] == -1){
-                    continue;
-                }
-                if ((m[sorIndex][oszlopIndex] == 1 || m[sorIndex][oszlopIndex] == 2) && (szomszédokOssz < 2 || szomszédokOssz > 3)){
-                    mFinal[sorIndex][oszlopIndex] = 0;
-                }
-                if (m[sorIndex][oszlopIndex] == 0 && (szomszédokOssz == 3)){
-                    if  (szomszédok1 > szomszédok2){
-                        mFinal[sorIndex][oszlopIndex] = 1;
-                    } else{
-                        mFinal[sorIndex][oszlopIndex] = 2;
-                    }
-                }
-                
-            }
+        if (m[sorIndex + i][oszlopIndex + j] == jatekosSzam) {
+          szomszedok++;
         }
-        this.#matrix = m;
-        return m;
+      }
     }
+
+    return szomszedok;
+  }
+
+  get #kovetkezoAllapot(): number[][] {
+    const m: number[][] = this.#matrix;
+    const mFinal: number[][] = this.#matrix;
+    for (let sorIndex = 0; sorIndex < this.#matrix.length; sorIndex++) {
+      for (let oszlopIndex = 0; oszlopIndex < this.#matrix[0].length; oszlopIndex++) {
+        const szomszédok1: number = this.#szomszedokSzama(sorIndex, oszlopIndex, 1);
+        const szomszédok2: number = this.#szomszedokSzama(sorIndex, oszlopIndex, 2);
+        const szomszédokOssz: number = szomszédok1 + szomszédok2;
+        if (m[sorIndex][oszlopIndex] == -1) {
+          continue;
+        }
+        if (
+          (m[sorIndex][oszlopIndex] == 1 || m[sorIndex][oszlopIndex] == 2) &&
+          (szomszédokOssz < 2 || szomszédokOssz > 3)
+        ) {
+          mFinal[sorIndex][oszlopIndex] = 0;
+        }
+        if (m[sorIndex][oszlopIndex] == 0 && szomszédokOssz == 3) {
+          if (szomszédok1 > szomszédok2) {
+            mFinal[sorIndex][oszlopIndex] = 1;
+          } else {
+            mFinal[sorIndex][oszlopIndex] = 2;
+          }
+        }
+      }
+    }
+    this.#matrix = m;
+    return m;
+  }
 
   get #megjelenit(): string {
     let vissza: string = "";
@@ -64,12 +62,19 @@ export default class Simulator {
       for (let oszlopIndex = 0; oszlopIndex < this.#matrix[0].length; oszlopIndex++) {
         const aktualis: number = this.#matrix[sorIndex][oszlopIndex];
 
-        if (aktualis == -1) {
+        if (
+          sorIndex == 0 ||
+          oszlopIndex == 0 ||
+          sorIndex == this.#matrix.length - 1 ||
+          oszlopIndex == this.#matrix[0].length - 1
+        ) {
           vissza += "X";
         } else if (aktualis == 0) {
           vissza += " ";
         } else if (aktualis == 1) {
           vissza += "S";
+        } else if (aktualis == 2) {
+          vissza += "K";
         }
       }
 
@@ -83,11 +88,10 @@ export default class Simulator {
     return this.#megjelenit;
   }
 
-    get run(){
-        this.#megjelenit;
-        this.#matrix = this.#kovetkezoAllapot
-        
-    }
+  get run() {
+    this.#megjelenit;
+    this.#matrix = this.#kovetkezoAllapot;
+  }
 
   lerak(sor: number, oszlop: number): void {
     if (this.#matrix[sor][oszlop] == -1) {
